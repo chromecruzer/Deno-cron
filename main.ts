9febc512c272b21f7eaca050d7b498c67a34bc57
chromecruzer <147481419+chromecruzer@users.noreply.github.com>
2024-08-21 10:36:40 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -68,7 +68,7 @@ async function checkMultipleUrls(urls: string[]) {
 }
 
 // Schedule the task to run every 15 minutes
-Deno.cron("Check Site Status", "*/15 * * * *", () => {
+Deno.cron("Check Site Status", "*/1 * * * *", () => {
   console.log('Checking site statuses...');
   checkMultipleUrls(urlsToCheck);
 });
